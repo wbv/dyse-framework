791520f627698b8eee1ac5c7c34f1a875dd85c61
Walter B. Vaughan <wbv@users.noreply.github.com>
2022-11-13 21:20:21 -0500

    augment nw_reg

--- dishsoap/src/nw_reg.vhdl
+++ dishsoap/src/nw_reg.vhdl
@@ -1,44 +1,65 @@
 --| nw_reg: an `N`-bit register outputing the current network state
---| 
+--|
 --| generics
 --| --------
---| N    number of elements 
---| 
+--| N    number of elements
+--|
 --| dir  port        port description
 --| ---  ----        ----------------
 --| out  state       current state of all network elements (as a std_logic_vector)
 --| in   state_next  what `state` will become at next clk
---| 
+--|
 --| in   clk         clock (leading edge)
 --| in   en          enable update
---| in   rst         asynchronous reset (active 'hi')
---| 
+--| in   arst         asynchronous reset (active 'hi')
+--|
 
 library ieee;
 use ieee.std_logic_1164.all;
 
 entity nw_reg is
 	generic (
-		N : natural := 3 
+		N : natural := 3
 	);
 	port (
-		state_next: in  std_logic_vector(N - 1 downto 0);
-		state:      out std_logic_vector(N - 1 downto 0);
+		--! computed next state for each element
+		--! (should connect to the output of the network combinational logic)
+		state_next: in std_logic_vector(N - 1 downto 0);
+
+		--! initial (reset) state of the network
+		init_state: in std_logic_vector(N - 1 downto 0);
+		--! mask for which rules are selected
+		rule_sel: in std_logic_vector(N - 1 downto 0);
+
+		--! forcing function:
+		--! when force_en = 1, override state of elements with force_vals if
+		--! they are selected by force_elems
+		force_en:    in std_logic;
+		force_elems: in std_logic_vector(N - 1 downto 0);
+		force_vals:  in std_logic_vector(N - 1 downto 0);
 
 		clk: in std_logic;
 		en:  in std_logic;
-		rst: in std_logic
+		arst: in std_logic;
+
+		state: out std_logic_vector(N - 1 downto 0)
 	);
 end nw_reg;
 
 architecture behavioral of nw_reg is
 begin
-	process(clk, rst)
+	process(clk, arst)
 	begin
-		if rst = '1' then
-			state <= (others => '0');
+		if arst = '1' then
+			state <= init_state;
 		elsif rising_edge(clk) and en = '1' then
-			state <= state_next;
+			for i in 0 to N-1 loop
+				if force_en = '1' and force_elems(i) = '1' then
+					state(i) <= force_vals(i);
+				elsif rule_sel(i) = '1' then
+					state(i) <= state_next(i);
+				end if;
+			end loop;
 		end if;
 	end process;
 end behavioral;
--- dishsoap/src/nw_reg_tb.vhdl
+++ dishsoap/src/nw_reg_tb.vhdl
@@ -11,24 +11,35 @@ end nw_reg_tb;
 architecture test of nw_reg_tb is
 	component nw_reg
 		generic (
-			N : natural := N 
+			N : natural := N
 		);
 		port (
-			state_next: in  std_logic_vector(N - 1 downto 0);
-			state:      out std_logic_vector(N - 1 downto 0);
-
-			clk: in std_logic;
-			en:  in std_logic;
-			rst: in std_logic
+			state_next:  in  std_logic_vector(N - 1 downto 0);
+			init_state:  in  std_logic_vector(N - 1 downto 0);
+			rule_sel:    in  std_logic_vector(N - 1 downto 0);
+			force_elems: in  std_logic_vector(N - 1 downto 0);
+			force_vals:  in  std_logic_vector(N - 1 downto 0);
+			force_en:    in  std_logic;
+			clk:         in  std_logic;
+			en:          in  std_logic;
+			arst:        in  std_logic;
+			state:       out std_logic_vector(N - 1 downto 0)
 		);
 	end component;
 	for uut: nw_reg use entity work.nw_reg;
 
 	-- uut signals
-	signal state_next, state: std_logic_vector(N - 1 downto 0);
-	signal en, rst: std_logic;
-	-- clock starts high to align rising edge to CLK_PERIOD intervals
-	signal clk: std_logic := '1';
+	signal state:       std_logic_vector(N - 1 downto 0);
+	signal state_next:  std_logic_vector(N - 1 downto 0);
+	signal init_state:  std_logic_vector(N - 1 downto 0);
+	signal rule_sel:    std_logic_vector(N - 1 downto 0);
+	signal force_elems: std_logic_vector(N - 1 downto 0);
+	signal force_vals:  std_logic_vector(N - 1 downto 0);
+	signal force_en:    std_logic;
+	signal en:          std_logic;
+	signal arst:        std_logic;
+	-- clock starts low to move rising edge off CLK_PERIOD intervals
+	signal clk: std_logic := '0';
 
 	-- flag for test completion
 	shared variable test_complete : boolean := false;
@@ -42,12 +53,16 @@ begin
 			N => N
 		)
 		port map (
-			state      => state,
-			state_next => state_next,
-
-			clk => clk,
-			en  => en,
-			rst => rst
+			state_next   => state_next,
+			init_state   => init_state,
+			rule_sel     => rule_sel,
+			force_elems  => force_elems,
+			force_vals   => force_vals,
+			force_en     => force_en,
+			clk          => clk,
+			en           => en,
+			arst         => arst,
+			state        => state
 		);
 
 -- clock: oscillate until test_complete
@@ -71,56 +86,84 @@ begin
 	-- initial states
 	state_next <= "1010";
 	en <= '0';
-	rst <= '0';
+	arst <= '0';
+	init_state <= (others => '0');
+	rule_sel <= (others => '1');
+	force_en <= '0';
+	force_elems <= (others => 'Z');
+	force_vals <= (others => 'Z');
 
 	wait for CLK_PERIOD;
-	rst <= '1';
+	arst <= '1', '0' after CLK_PERIOD;
 	wait for CLK_PERIOD;
-	rst <= '0';
 
 	assert state = "0000"
 		report "RST did not reset state";
 
 
+	en <= '1', '0' after CLK_PERIOD;
 	wait for CLK_PERIOD;
-	en <= '1';
-	wait for CLK_PERIOD;
-	en <= '0';
 
 	assert state = "1010"
 		report "EN did not update state";
 
 
-	wait for CLK_PERIOD;
 	state_next <= "0011";
-	en <= '1';
+	en <= '1', '0' after CLK_PERIOD;
 	wait for CLK_PERIOD;
-	en <= '0';
 
 	assert state = "0011"
 		report "EN did not update state";
 
 
+	arst <= '1', '0' after CLK_PERIOD;
 	wait for CLK_PERIOD;
-	rst <= '1';
-	wait for CLK_PERIOD;
-	rst <= '0';
 
 	assert state = "0000"
 		report "RST did not reset state";
 
 
+	en <= '0', '1' after CLK_PERIOD;
+	arst <= '1', '0' after CLK_PERIOD;
 	wait for CLK_PERIOD;
-	en <= '0';
-	rst <= '1';
-	wait for CLK_PERIOD;
-	en <= '1';
-	rst <= '0';
 
 	assert state = "0000"
 		report "RST did not reset state";
 
 
+	-- new test: reset to custom 'init'
+	init_state <= "0111";
+	arst <= '1', '0' after CLK_PERIOD;
+	wait for CLK_PERIOD;
+
+	assert state = "0111";
+		report "state not updated to nonzero init";
+
+
+	-- new test: partial update
+	rule_sel <= "0001";
+	state_next <= "1000";
+	en <= '1', '0' after CLK_PERIOD;
+	wait for CLK_PERIOD;
+
+	assert state = "0110"
+		report "rule_sel failed first update";
+
+
+	-- new test: partial update (continued)
+	rule_sel <= "1100";
+	state_next <= "1001";
+	en <= '1', '0' after CLK_PERIOD;
+	wait for CLK_PERIOD;
+
+	assert state = "1010"
+		report "rule_sel failed first update";
+
+	-- TODO tests: forcing functions
+
+	--|================|--
+	--| Test Completed |--
+	--|================|--
 	wait for CLK_PERIOD;
 	test_complete := true;
 	wait;
